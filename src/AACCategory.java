367cc3b1cf493cef518a2924f09558f6ed42e9bb
yusufbon <yusufbonsen@gmail.com>
2025-01-03 23:12:49 -0600

    Update AACCategory.java

--- src/AACCategory.java
+++ src/AACCategory.java
@@ -1,13 +1,26 @@
 import java.util.NoSuchElementException;
+import edu.grinnell.csc207.util.*;
 
 /**
  * Represents the mappings for a single category of items that should
  * be displayed
  * 
- * @author Catie Baker & YOUR NAME HERE
+ * @author Catie Baker & Bonsen Yusuf
  *
  */
 public class AACCategory implements AACPage {
+
+	/*Fields */
+
+	/**
+     * The name of the category.
+     */
+    private String name; // The name of the category
+
+    /**
+     * The mappings from image locations to text for this category.
+     */
+    private AssociativeArray<String, String> items; // Map of image locations to text
 
 	
 	/**
@@ -15,17 +28,25 @@ public class AACCategory implements AACPage {
 	 * @param name the name of the category
 	 */
 	public AACCategory(String name) {
-
-	}
+		this.name = name;
+        this.items = new AssociativeArray<>();
+	} // AACCategory(String)
 	
+    /*Public Methods */
+
 	/**
 	 * Adds the image location, text pairing to the category
 	 * @param imageLoc the location of the image
 	 * @param text the text that image should speak
 	 */
 	public void addItem(String imageLoc, String text) {
-
-	}
+		try {
+            items.set(imageLoc, text);
+        } catch (Exception e) {
+            // Handle exceptions like NullKeyException if needed
+            System.err.println("Error adding item: " + e.getMessage());
+        } // try/catch
+	}//addItem(String imageLoc, String text)
 
 	/**
 	 * Returns an array of all the images in the category
@@ -33,16 +54,21 @@ public class AACCategory implements AACPage {
 	 * it should return an empty array
 	 */
 	public String[] getImageLocs() {
-		return null;
-	}
+		Object[] keys = items.getKeys(); // Get keys as Object[]
+    	String[] stringKeys = new String[keys.length]; // Create a String array
+    	for (int i = 0; i < keys.length; i++) {
+        	stringKeys[i] = (String) keys[i]; // Cast each key to String
+    	} // for
+    	return stringKeys;
+	} //getImageLocs()
 
 	/**
 	 * Returns the name of the category
 	 * @return the name of the category
 	 */
 	public String getCategory() {
-		return "";
-	}
+		return name;
+	}//getCategory()
 
 	/**
 	 * Returns the text associated with the given image in this category
@@ -52,8 +78,12 @@ public class AACCategory implements AACPage {
 	 * 		   category
 	 */
 	public String select(String imageLoc) {
-		return "";
-	}
+		try {
+            return items.get(imageLoc);
+        } catch (KeyNotFoundException e) {
+            throw new NoSuchElementException("Image not found: " + imageLoc);
+        } // try/catch
+	} //select()
 
 	/**
 	 * Determines if the provided images is stored in the category
@@ -61,6 +91,6 @@ public class AACCategory implements AACPage {
 	 * @return true if it is in the category, false otherwise
 	 */
 	public boolean hasImage(String imageLoc) {
-		return false;
-	}
-}
+		return items.hasKey(imageLoc);
+	} //hasImage()
+} //AACCategory
